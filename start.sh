11bd09ce6eb55cd511a993f82e166bccf3767d8a
Tom Nussbaumer <thomas.nussbaumer@gmx.net>
2015-09-04 14:16:51 +0200

    remove my_init (now done via ENTRYPOINT) and add more docs

--- start.sh
+++ start.sh
@@ -8,12 +8,17 @@
 #
 # example:  
 #
-# ./start.sh -v $(pwd):/project
+# ./start.sh -v $(pwd):/project -p 80:80 -p 443:443
 #
-#    mounts the current working directory to directory /project within the
-#    container. Note that the UID and GID of user app  will be remapped to
-#    match the UID/GUID of the user executing this script.
+# (*) mounts the current working directory to directory /project within the
+#     container
+# (*) maps host port 80 and 443 on any interface to container ports 80 and 443
+#
+#    Note that the UID and GID of user app  will be remapped to match the
+#    UID/GUID of the user executing this script.
+#
+#    The init system (my_init --) will be started via ENTRYPOINT
 #
 ###############################################################################
-docker run -ti --rm -p 30000:80 "$@" $(cat REPO_AND_VERSION) \
-       /sbin/my_init -- /sbin/remapuser app $(id -u) $(id -g) bash 
+docker run -ti --rm "$@" $(cat REPO_AND_VERSION) \
+       remapuser app $(id -u) $(id -g) bash
